c29d4527edae59c35dc998ec42b3f6a22dfee40a
rob <rob.baldauf@googlemail.com>
2021-07-11 20:42:32 +0200

    restructured code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # photo-mosaic-mgmt-ui
 A single page standalone React JS UI for the photo-mosaic API service
+
+
+
+## Install
+
+```shell
+$ sudo apt install python3.7
+```
+
+
+## Run service
+
+## License
+
+[GPL-3.0 License](LICENSE)
--- config/config.js
+++ config/config.js
@@ -3,4 +3,4 @@
 // UI Configuration
 export const API_KEY =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHAiOiJwaXhlbC1tb3NhaWMiLCJpZCI6ImFkbWluIiwiZXhwIjoxNjg0ODY4OTE5fQ.xoU50xWlmVHDkdmPk0A1v9GV4tN768Cv7ExsiT9d53s";
-export const API_SERVER = "http://192.168.254.215:8111";
+export const API_SERVER = "http://127.0.0.1:8111";
